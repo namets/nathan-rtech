625b39f66015b36524eaf4a1d164817b3cf01dd8
Nathan <nathanmetsala@gmail.com>
2019-05-31 21:12:21 +0300

    changes and updates

--- src/range_visualizer/src/range_publisher.cpp
+++ src/range_visualizer/src/range_publisher.cpp
@@ -9,8 +9,8 @@ ros::init(argc, argv, "range_publisher");
 
 ros::NodeHandle n;
 
-ros::Publisher range_publisher= n.advertise<sensor_msgs::Range>("fake_range",1000);
-ros::Rate loop_rate(10);
+ros::Publisher range_publisher= n.advertise<sensor_msgs::Range>("fake_range",1000);	
+ros::Rate loop_rate(10);	//Loop rate
 	
 
 int count =0;
@@ -20,14 +20,13 @@ while(ros::ok())
 	sensor_msgs::Range sensor;
 	std_msgs::Header header;
 	header.frame_id="range_test";
-	sensor.min_range=0.02;
-	sensor.max_range=4;
-
-	sensor.field_of_view=0.785398;
+	sensor.min_range=0.02;		// In meters
+	sensor.max_range=4;		// In meters
+	sensor.field_of_view=0.785398; // In radians
 
 //Assign message 
 	sensor.header=header;
-	sensor.range = ((float)rand()/RAND_MAX)*4;
+	sensor.range = ((float)rand()/RAND_MAX)*4; 
 
 	ROS_INFO("%f",sensor.range);
 
